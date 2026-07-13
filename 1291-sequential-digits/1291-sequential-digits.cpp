5542d08f8e21ec87a93f96cacd2f25df615b7123
Arya Verma <aryaverma888@gmail.com>
2026-07-13 18:41:31 +0530

    Time: 0 ms (100%), Space: 8.4 MB (48.96%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.cpp
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -6,7 +6,7 @@ public:
         string l = to_string(low);
         string h = to_string(high);
         for (int i = l.size(); i <= h.size(); i++) {
-            for (int j = 0; j + i <= 9; j++) {
+            for (int j = 0; j <= 9 - i; j++) {
                 int num = stoi(s.substr(j, i));
                 if (num >= low && num <= high) {
                     res.push_back(num);
