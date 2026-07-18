f4f3b85a12a449586354857c07d026ef77542513
Arya Verma <aryaverma888@gmail.com>
2026-07-18 21:49:19 +0530

    Time: 4 ms (14.53%), Space: 10.2 MB (24.65%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string ans;
+        for (char c : s) {
+            if (isalnum(c)) {
+                ans += tolower(c);
+            }
+        }
+        int l = 0;
+        int r = ans.size() - 1;
+        while (l < r) {
+            if (ans[l] != ans[r]) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+};
