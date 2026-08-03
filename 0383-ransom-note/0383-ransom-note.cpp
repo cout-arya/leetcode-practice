857cb9d437754553fa647b0deb445383ec125d49
Arya Verma <aryaverma888@gmail.com>
2026-08-04 01:10:35 +0530

    Time: 6 ms (25.5%), Space: 11.8 MB (49.71%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char, int> mp;
+        for (char c : magazine) {
+            mp[c]++;
+        }
+        for (char ch : ransomNote) {
+            if (mp[ch] > 0) {
+                mp[ch]--;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+};
